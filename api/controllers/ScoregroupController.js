51532076045b04e815d03e1b0563ae6376b734f3
scan.baxin.office <suecher@qq.com>
2016-10-18 21:47:56 +0800

    增加新的模型文件

---
+++ api/controllers/ScoregroupController.js
@@ -0,0 +1,11 @@
+/**
+ * ScoregroupController
+ *
+ * @description :: Server-side logic for managing scoregroups
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
